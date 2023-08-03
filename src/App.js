4d6f826b41649dc78c081ddfcd1d82320fd500c4
edward tully <edwardTully1998@gmail.com>
2023-08-02 21:39:33 -0500

    mod page title

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     
     <div className="App">
-      <h1>JSON Data, React, Redux-Toolkit</h1>
+      <h1>JSON Data, React, Redux-Toolkit, Axios</h1>
       <br></br>
       <CompanyView/>
       <UsersView/>
